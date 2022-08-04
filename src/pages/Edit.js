c762c7d19bfdecbc95d6d9727c0878c3f2f54398
junseokoh-hub <lunulatecat@naver.com>
2022-08-04 16:37:16 +0900

    change titles

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -24,6 +24,11 @@ function Edit() {
     }
   }, [id, diaryList, navigate]);
 
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정 일기장 - ${id}번 일기 수정`;
+  }, []);
+
   return (
     <div>
       {originData && <DiaryEditor isEdit={true} originData={originData} />}
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DiaryEditor from "../Components/DiaryEditor";
 
 function New() {
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정 일기장 - 새 일기`;
+  }, []);
   return (
     <div>
       <DiaryEditor />
